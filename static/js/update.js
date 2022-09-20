8888acf6f71098e9c849dd5d7dab67930286dad4
rakkyo150 <86054813+rakkyo150@users.noreply.github.com>
2022-09-20 17:12:15 +0900

    クリーンアップ

--- static/js/update.js
+++ static/js/update.js
@@ -1,42 +1,46 @@
-$(function(){
+$(function () {
     $.ajax({
-        url: '/init',
-        type: 'post',
-        data: '読み込み開始'
-    }).done(function(data){
-        console.log(data);
-        $("div.nowLoading").css("display","none")
-        $("div.finishLoading").css("display","block")
-    }).fail(function(){
-        console.log('failed');
-    });
+        url: "/init",
+        type: "post",
+        data: "読み込み開始",
+    })
+        .done(function (data) {
+            console.log(data);
+            $("div.nowLoading").css("display", "none");
+            $("div.finishLoading").css("display", "block");
+        })
+        .fail(function () {
+            console.log("failed");
+        });
 
-    $("input.submit").click(function(){
-        console.log('checkValue')
+    $("input.submit").click(function () {
+        console.log("checkValue");
         const checkValue = $('input:radio[name="mode"]:checked').val();
-        console.log(checkValue)
+        console.log(checkValue);
 
-        console.log('postData')
-        const postData={"mode":checkValue,"input":$('input.input').val()};
+        console.log("postData");
+        const postData = { mode: checkValue, input: $("input.input").val() };
 
         $.ajax({
-            url: '/predict',
-            type: 'post',
+            url: "/predict",
+            type: "post",
             data: postData,
-        }).done(function(data){
-            let resultHtml="";
-            console.log(data);
-            Object.keys(data).forEach(function(key){
-                resultHtml+=`<p>${key} : ${this[key]}</p>\n`
-            },data)
-            $("h1.resultMain").html(resultHtml)
-            $("div.errorPredict").css("display","none")
-            $("div.finishPredict").css("display","block")
-        }).fail(function(){
-            $("h1.errorSentence").text("Your input is OK?")
-            $("div.finishPredict").css("display","none")
-            $("div.errorPredict").css("display","block")
-            console.log('failed');
-        });
-    })
+        })
+            .done(function (data) {
+                let resultHtml = "";
+                console.log(data);
+                Object.keys(data).forEach(function (key) {
+                    resultHtml += `<p>${key} : ${this[key]}</p>\n`;
+                }, data);
+                $("h1.resultMain").html(resultHtml);
+                $("div.errorPredict").css("display", "none");
+                $("div.finishPredict").css("display", "block");
+            })
+            .fail(function () {
+                $("h1.errorSentence").text("Your input is OK?");
+                $("div.finishPredict").css("display", "none");
+                $("div.errorPredict").css("display", "block");
+                console.log("failed");
+            });
+    });
 });
